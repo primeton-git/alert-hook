1279a799cebd642152c397e11ed783ba5dc5392f
Harmony <earth@sun.galaxy>
2021-02-24 16:17:54 +0800

    Init

---
+++ src/main/java/com/gaoya/mdm/webhook/MdmWebhookApplication.java
@@ -0,0 +1,13 @@
+package com.gaoya.mdm.webhook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MdmWebhookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MdmWebhookApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gaoya/mdm/webhook/MdmWebhookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaoya.mdm.webhook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MdmWebhookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
